b0344dcfc4b50e780b4da71e5564259645bb79ce
tpm909 <ankitoperez909@gmail.com>
2024-09-30 16:27:23 -0300

    ahora si

--- Projecto_Transversal/src/main/java/com/mycompany/projecto_transversal/AlumnoData.java
+++ Projecto_Transversal/src/main/java/com/mycompany/projecto_transversal/AlumnoData.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package projecto_transversal;
+package com.mycompany.projecto_transversal;
 
 /**
  *
  * @author franco
  */
-public class Alumno {
+public class AlumnoData {
     
 }
--- Projecto_Transversal/src/main/java/com/mycompany/projecto_transversal/Conexion.java
+++ Projecto_Transversal/src/main/java/com/mycompany/projecto_transversal/Conexion.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package projecto_transversal;
+package com.mycompany.projecto_transversal;
 
 /**
  *
